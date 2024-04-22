69cd746a7db6b7a94114f9d8336eec358d1f006b
SamoP <samo.pinter@gmail.com>
2024-04-22 11:51:50 +0200

    Sticky navbar, index.md clean up

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -5,3 +5,20 @@ import "$styles/syntax-highlighting.css"
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
+
+
+
+  const container = document.querySelector('.dropdown-selection');
+  const dropdown = container.querySelector('sl-dropdown');
+  dropdown.addEventListener('sl-select', event => {
+    const selectedItem = event.detail.item;
+    console.log(selectedItem.value);
+  });
+
+  function onLoadDrawer(){
+    const drawer = document.querySelector('.drawer-overview');
+    const openButton = drawer.nextElementSibling;
+    const closeButton = drawer.querySelector('sl-button[variant="primary"]');
+    openButton.addEventListener('click', () => drawer.show());
+    closeButton.addEventListener('click', () => drawer.hide());
+  }
